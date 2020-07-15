0c5352aa88d3530a0e866edbb12a4695bcafdca1
Rois Faozi <67546196+Roisfaozi@users.noreply.github.com>
2020-07-16 06:54:28 +0700

    Initial commit

---
+++ app/src/main/java/com/raven/myactionbar/MenuActivity.kt
@@ -0,0 +1,39 @@
+package com.raven.myactionbar
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.view.*
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu1 -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, MenuFragment())
+                    .addToBackStack(null)
+                    .commit()
+                return true
+            }
+            R.id.menu2 -> {
+                val i = Intent(this, MenuActivity::class.java)
+                return true
+            }
+            else -> return true
+        }
+
+    }
+}
